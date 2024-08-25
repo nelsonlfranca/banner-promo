abbfc25159faa66f47e160a6b37aad4c1bfc5d37
nelsonlfranca <nelsonleandro8888@gmail.com>
2024-08-25 20:46:17 -0300

    Pronto-Sujeito alteração

--- src/app/page/banner.jsx
+++ src/app/page/banner.jsx
@@ -1,30 +1,65 @@
-// components/Banner.js
-
 export default function Banner() {
   return (
-    <div className="relative w-full h-64 sm:h-96 bg-cover bg-center" style={{ backgroundImage: "url('/BANNER-PROMOCAO.jpg')" }}>
-      <div className="absolute inset-0 bg-gradient-to-r from-transparent to-blue-700 opacity-70"></div>
-      <div className="relative z-10 flex items-center justify-between h-full px-6 sm:px-12">
-        <div>
-          <h1 className="text-4xl sm:text-6xl font-bold text-white leading-tight">
-            OFERTA <span className="text-blue-400">VÁLIDA</span> <br />
-            POR POUCOS DIAS
+    <div className="flex w-full">
+      
+      {/* Moeda à esquerda */}
+      <img
+        src="https://facilita-banner-images.s3.sa-east-1.amazonaws.com/banner-promo/moeda+esquerda+baixo.png"
+        className="absolute top-[190px] left-[1px] sm:right-[-120px] sm:bottom-[-40px] h-[80px] sm:h-[140px]"
+        alt="Moeda"
+      />
+
+      {/* Seção Branca */}
+      <div className="bg-white w-[40%] sm:h-full flex flex-col items-center justify-center p-4 sm:p-8">
+          
+        <div className="py-5 flex flex-col items-center text-center sm:text-left">
+          <h1 className="text-2xl sm:text-7xl font-bold text-black leading-tight">
+            OFERTA<br/> <span className="text-[#0367fd]">VÁLIDA</span>
           </h1>
-          <button className="mt-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg">
+          <p className="text-3xl text-black font-bold">POR POUCOS DIAS</p>
+          
+          <button className="mt-4 bg-gradient-to-r from-[#19136d] to-[#0201f3] text-white font-semibold py-3 px-6 rounded-lg">
             QUERO MUDAR PARA O PREMIUM
           </button>
+          
         </div>
-        <div className="text-right text-white">
-          <p className="text-lg sm:text-2xl">
+     
+      </div>
+       
+       {/*Seção da Imagem*/}
+      <div className="absolute top-[26.6%] left-[calc(35%-50px)] transform -translate-y-1/2 z-20">
+        <img className="max-w-[347px]"
+        src="https://facilita-banner-images.s3.sa-east-1.amazonaws.com/banner-promo/erikk.png"
+        />
+       </div>
+        
+      {/* Seção Azul */}
+      <div className="bg-gradient-to-r from-[#19136d] to-[#0201f3] w-[60%] sm:w-full flex-1 flex flex-col items-center p-4 sm:p-8">
+        
+        <div>
+          <p className="text-white text-3xl pt-10 pr-10">
             Prepare seu escritório para entrar em <br />
             uma nova fase com o <strong>Pacote Premium!</strong>
           </p>
-          <p className="mt-4 text-2xl sm:text-3xl font-bold">
-            Adquira hoje com <span className="text-yellow-400">10% OFF</span><br />
+          <p className="text-center py-10 pr-16 font-bold text-3xl">
+            Adquira <span className="font-extralight">hoje</span> com 10% OFF<br />
             + 1 antecipação de 24 horas.
           </p>
         </div>
+
       </div>
+      {/* Moedas à direita */}
+      <img
+        src="https://facilita-banner-images.s3.sa-east-1.amazonaws.com/banner-promo/moeda+direita+cima.png"
+        className="absolute top-[-2px] left-[1326px] sm:right-[-120px] sm:bottom-[-40px] h-[0px] sm:h-[110px]"
+        alt="Moeda"
+      />
+
+      <img
+        src="https://facilita-banner-images.s3.sa-east-1.amazonaws.com/banner-promo/moeda+direta+baixo.png"
+        className="absolute top-[210px] left-[1190px] sm:right-[-120px] sm:bottom-[-40px] h-[80px] sm:h-[140px]"
+        alt="Moeda"
+      />
     </div>
   );
 }
